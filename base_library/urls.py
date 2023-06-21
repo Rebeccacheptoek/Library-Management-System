a1ac607ef634e121752fafb519d64f3ee6086f32
Rebeccacheptoek <rebbytoek095@gmail.com>
2023-06-21 18:08:31 +0300

    main files

--- base_library/urls.py
+++ base_library/urls.py
@@ -5,9 +5,7 @@ urlpatterns = [
     path('home', views.index, name="home"),
     path('book', views.book_index, name="book"),
     path('add_book', views.add_book, name="add-book"),
+    path('update_book', views.update_book, name="update-book"),
     path('add_member', views.add_member, name="add-member"),
-    # path('login', views.login_user, name="login"),
-    # path('register', views.register, name="register"),
-    # path('logout', views.logout_user, name="logout"),
-
+    path('update_member', views.update_member, name="update-member"),
 ]
